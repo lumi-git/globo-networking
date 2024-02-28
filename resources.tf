ef3b2c4b5ca55bfd430ed2ac2d8a3a5051b3c0d6
ronan <ronan.tremoureux@gmail.com>
2024-02-28 10:35:30 +0100

    fmt

--- resources.tf
+++ resources.tf
@@ -19,7 +19,7 @@ locals {
   common_tags = {
     environment = var.environment
     billingCode = var.billing_code
-    Workspace = terraform.Workspace
+    Workspace   = terraform.Workspace
   }
 }
 
