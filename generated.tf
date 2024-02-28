5597672062ca1a4ed349e1c7c359e65b05a1be32
ronan <ronan.tremoureux@gmail.com>
2024-02-28 08:43:22 +0100

    Initial commit

---
+++ generated.tf
@@ -0,0 +1,4 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform from "sg-027d5390c805f7554"
---
+++ imports.tf
@@ -0,0 +1,49 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+/*
+import {
+  to = module.main.aws_vpc.this[0]
+  id = "vpc-0eb3134ae033eac45" #VPC
+}
+
+import {
+  to = module.main.aws_subnet.public[0]
+  id = "subnet-08f1ec6dc11f2a896" #PublicSubnet1
+}
+
+import {
+  to = module.main.aws_subnet.public[1]
+  id = "subnet-03a45a5e49c085b13" #PublicSubnet2
+}
+
+import {
+  to = module.main.aws_internet_gateway.this[0]
+  id = "igw-0220a349cb60ebf66" #InternetGateway
+}
+
+import {
+  to = module.main.aws_route.public_internet_gateway[0]
+  id = "rtb-03bbb89ddc484c9c0_0.0.0.0/0" #DefaultPublicRoute
+}
+
+import {
+  to = module.main.aws_route_table.public[0]
+  id = "rtb-03bbb89ddc484c9c0" #PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[0]
+  id = "subnet-08f1ec6dc11f2a896/rtb-03bbb89ddc484c9c0" #PublicSubnet1/PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-03a45a5e49c085b13/rtb-03bbb89ddc484c9c0" #PublicSubnet2/PublicRouteTable
+}
+
+import {
+  to = aws_security_group.ingress
+  id = "sg-027d5390c805f7554" #NoIngressSecurityGroup
+}
+*/
